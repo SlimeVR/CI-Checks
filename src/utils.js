5f82329258e85607828071aced6c374701d531cb
ImUrX <urielfontan2002@gmail.com>
2024-04-04 03:41:39 -0300

    init project

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+export const SOLARXR_PR_REGEX =
+	/https:\/\/github\.com\/SlimeVR\/SolarXR-Protocol\/pull\/(\d+)/i;
+export const CI_CHECK_NAME = "solarxr_check";
+export const CI_CHECK_TITLE = "SolarXR Check";
